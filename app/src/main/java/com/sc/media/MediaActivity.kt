9d3dff5c4ac9627015a8b66471d32cb4e3ca81ee
SSSxCCC <570909477@qq.com>
2021-04-29 15:24:09 +0800

    Add open media file button.
    Signed-off-by: SSSxCCC <570909477@qq.com>

--- app/src/main/java/com/sc/media/MediaActivity.kt
+++ app/src/main/java/com/sc/media/MediaActivity.kt
@@ -3,30 +3,43 @@ package com.sc.media
 import android.content.Context
 import android.content.Intent
 import android.media.MediaPlayer
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.SurfaceHolder
 import android.view.SurfaceView
+import android.widget.Button
 import com.sc.scapp.R
 
 class MediaActivity : AppCompatActivity() {
+    var mediaPlayer: MediaPlayer? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_media)
 
-        val intent = Intent(Intent.ACTION_GET_CONTENT)
-        intent.setType("*/*")
-        intent.addCategory(Intent.CATEGORY_OPENABLE)
-        startActivityForResult(intent, 0)
+        val openMediaFileButton = findViewById<Button>(R.id.open_media_file_button)
+        openMediaFileButton.setOnClickListener {
+            val intent = Intent(Intent.ACTION_GET_CONTENT).apply {
+                type = "*/*"
+                addCategory(Intent.CATEGORY_OPENABLE)
+            }
+            startActivityForResult(intent, 0)
+        }
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
-        val uri = data?.data ?: return
+        val uri: Uri = data?.data ?: return
         val surfaceView = findViewById<SurfaceView>(R.id.surface_view)
         surfaceView.holder.addCallback(object : SurfaceHolder.Callback {
             override fun surfaceCreated(holder: SurfaceHolder) {
-                MediaPlayer.create(this@MediaActivity, uri, holder).start()
+                mediaPlayer?.apply {
+                    stop()
+                    release()
+                }
+                mediaPlayer = MediaPlayer.create(this@MediaActivity, uri, holder)
+                mediaPlayer?.start()
             }
             override fun surfaceChanged(holder: SurfaceHolder, format: Int, width: Int, height: Int) { }
             override fun surfaceDestroyed(holder: SurfaceHolder) { }
